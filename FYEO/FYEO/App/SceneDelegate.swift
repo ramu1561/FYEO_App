8d22bc217a17bf7b3d08190fb5f5aaa1c58b594f
Ramu Pidatala <49405215+ramu1561@users.noreply.github.com>
2026-01-25 16:53:17 +0530

    Authentication design

--- FYEO/FYEO/App/SceneDelegate.swift
+++ FYEO/FYEO/App/SceneDelegate.swift
@@ -10,13 +10,31 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
 
+        window = UIWindow(windowScene: windowScene)
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let storyboard = UIStoryboard(
+            name: "Authentication",
+            bundle: nil
+        )
+
+        let signInVC = storyboard.instantiateViewController(
+            withIdentifier: "SignInViewController"
+        )
+
+        let navController = UINavigationController(
+            rootViewController: signInVC
+        )
+
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
